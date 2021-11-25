6efe6f036279d64f6bba789ed8eeb1f602f6c1db
Nazar <klymko1992@gmail.com>
2021-11-26 00:26:29 +0200

    task number 3 done

--- homework.js
+++ homework.js
@@ -10,6 +10,32 @@
 //    and return a string with car registration number, branch and color.
 //    Please make examples for three different cars
 
+class Car {
+    constructor(registrationNumber, brand, color, ownerName) {
+        this.registrationNumber = registrationNumber;
+        this.brand = brand;
+        this.color = color;
+        this.ownerName = ownerName;
+    }
+}
+const car1 = new Car("AT 9909 EI", "VW", "red", "Oleg");
+const car2 = new Car("BC 7281 OA", "Ford", "green", "Anton");
+const car3 = new Car("ZT 1909 EI", "MAZDA", "red", "Alex");
+
+const cars = [car1, car2, car3];
+
+function displayCarDetails(ownerName) {
+    const car = cars.find((car) => car.ownerName === ownerName);
+
+    return `registrationNumber: ${car.registrationNumber}, brand: ${car.brand}, color: ${car.color}`;
+}
+
+displayCarDetails("Anton");
+displayCarDetails("Alex");
+displayCarDetails("Oleg");
+
+
+
 // 4.We want this code to log out “hey amy”, but it logs out “hey arnold” - fix that
 
 // function greet (person) {
