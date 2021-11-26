dc7878c6ed0b47f466e5f3a5ba0fea21dc696ff9
nazar <klymko1992@gmail.com>
2021-11-26 09:37:27 +0200

    tasks homework done

--- homework.js
+++ homework.js
@@ -4,7 +4,20 @@
 // 1. Compute the exponent of a number (using recursion);
 //8^2 = 8 x 8 = 64
 
+function pow(x, n) {
+    return (n != 1) ? x*pow(x, n-1) : x;
+}
+console.log( pow(8, 2) )
+
 // 2. Write functions min and max that will find min and max number in array using apply
+
+let numbers = [3, 10, 43, 6, 7];
+
+let max = Math.max.apply(null, numbers);
+let min = Math.min.apply(null, numbers);
+
+console.log(max);
+console.log(min);
 
 // 3. write a function displayCarDetails that will receive as argument ownerName
 //    and return a string with car registration number, branch and color.
@@ -48,9 +61,26 @@ displayCarDetails("Oleg");
 // const amy = {name: 'amy'}
 // greet(amy)
 
+// code already logs “hey amy”!
+
 // 5.We want this code to log out the numbers 0, 1, 2, 3 in that order,
 // but it doesn’t do what we expect - fix that(find two solutions)
 
 // for (var i = 0; i < 4; i++) {
 //   setTimeout(() => console.log(i), 0)
 // }
+
+for (let i = 0; i < 4; i++) {
+    setTimeout(() => console.log(i), 0)
+}
+
+for (var i = 0; i < 4; i++) {
+    console.log(i)
+}
+
+function setDelay(i) {
+    setTimeout(() => console.log(i), 0);
+}
+for (var i = 0; i < 4; i++) {
+    setDelay(i);
+}
